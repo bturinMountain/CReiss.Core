a87bdc1ead9ac39ff1bd0900ef91616854afe46d
bturin <benjamin.turin@azurerome.com>
2025-07-28 00:48:59 -0400

    Import File View - Interim checkin

---
+++ Import/DailyImport.cs
@@ -0,0 +1,10 @@
+﻿namespace CReiss.Core.Import
+{
+
+    public class DailyImport
+    {
+        public DateTime ImportDateLocal { get; set; } = DateTime.MinValue;
+        public List<ImportFile> ImportFiles { get; set; } = new List<ImportFile>();
+    }
+
+}
---
+++ Import/ImportFile.cs
@@ -0,0 +1,34 @@
+﻿namespace CReiss.Core.Import
+{
+
+    public class ImportFile
+    {
+
+        private const string DIR_YEAR_FORMAT = "yyyy";
+        private const string DIR_MONTH_FORMAT = "MM MMMM";
+        private const string FILE_FORMAT = "yyyy-MM-dd hh-mm-ss";
+        private const string FILE_EXTENSION = "csv";
+
+        public int Id { get; set; } = 0;
+        public DateTime ReceviedOn { get; set; } = DateTime.MinValue;
+        public DateTime ImportFileDate { get; set; } = DateTime.MinValue;
+
+
+        public string FilePath
+        {
+            get
+            {
+                return String.Concat(this.ReceviedOn.ToString(DIR_YEAR_FORMAT), "\\", this.ReceviedOn.ToString(DIR_MONTH_FORMAT), "\\", this.FileName);
+            }
+        }
+        public string FileName
+        {
+            get
+            {
+                return String.Concat(this.ReceviedOn.ToString(FILE_FORMAT), ".", FILE_EXTENSION);
+            }
+        }
+
+    }
+
+}
