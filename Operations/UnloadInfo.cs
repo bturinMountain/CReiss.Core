0dafe4575fe721a1b23aabc4ba178267058e1b0b
bturin <benjamin.turin@azurerome.com>
2025-08-18 13:30:20 -0400

    Add Operations Unload pages
    Related Work Items: #142, #141

---
+++ Operations/UnloadInfo.cs
@@ -0,0 +1,19 @@
+﻿using CReiss.Core.Locations;
+
+namespace CReiss.Core.Operations
+{
+
+    public class UnloadInfo
+    {
+        public int Id { get; set; } = 0;
+        public Location UnloadLocation { get; private set; } = new Location();
+        public short UnloadedUnits { get; set; } = 0;
+        public short TrucksToDayPad { get; set; } = 0;
+        public short TrucksToStockPile { get; set; } = 0;
+        public string Notes { get; set; } = String.Empty;
+        public string SubmittedBy { get; set; } = String.Empty;
+        public DateTime SubmittedOnUtc {  get; set; } = DateTime.MinValue;
+
+    }
+
+}
