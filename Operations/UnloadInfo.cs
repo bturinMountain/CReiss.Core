50b1e26ed0777e7fc87433678e03ef2f116259cb
bturin <benjamin.turin@azurerome.com>
2025-09-03 19:25:10 -0400

    Change trucks to units/railcars
    Related Work Items: #151

--- Operations/UnloadInfo.cs
+++ Operations/UnloadInfo.cs
@@ -8,8 +8,8 @@ namespace CReiss.Core.Operations
         public int Id { get; set; } = 0;
         public Location UnloadLocation { get; private set; } = new Location();
         public short UnloadedUnits { get; set; } = 0;
-        public short TrucksToDayPad { get; set; } = 0;
-        public short TrucksToStockPile { get; set; } = 0;
+        public short UnitsToDayPad { get; set; } = 0;
+        public short UnitsToStockPile { get; set; } = 0;
         public string Notes { get; set; } = String.Empty;
         public string SubmittedBy { get; set; } = String.Empty;
 
