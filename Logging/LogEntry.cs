cddd79b9c9614a06e81904d1bd13f89b4b3940ff
bturin <bturin@mountainsoft.com>
2025-10-12 07:56:59 -0400

    Add additional logging features
    Related Work Items: #160

---
+++ Logging/LogEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace CReiss.Core.Logging
+{
+
+    public class LogEntry
+    {
+        public short LogLevel { get; set; } = 0;
+        public string Action { get; set; } = String.Empty;
+        public DateTime ActionDateTime { get; set; } = DateTime.MinValue;
+    }
+
+}
