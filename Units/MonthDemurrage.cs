341548959b35ef8ce765cea542a91dd50a4046de
bturin <benjamin.turin@azurerome.com>
2026-02-15 12:09:16 -0500

    Superior Demurrage widget added to dashboard and daily e-mail
    Related Work Items: #176

---
+++ Units/MonthDemurrage.cs
@@ -0,0 +1,17 @@
+﻿namespace CReiss.Core.Units
+{
+
+    public class MonthDemurrage
+    {
+        public string Month { get; set; } = String.Empty;
+        public List<DemurrageDay> Days { get; set; } = new List<DemurrageDay>();
+    }
+
+    public class DemurrageDay
+    {
+        public DateTime EventDate { get; set; }
+        public int UnitsBnsf { get; set; }
+        public int UnitsCReiss { get; set; }
+    }
+
+}
