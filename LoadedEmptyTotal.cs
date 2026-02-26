19282c67678515a8ea094312ac4422366d8cd57c
bturin <benjamin.turin@azurerome.com>
2026-02-26 13:32:23 -0500

    Unit/Dwell Time pop-up
    Related Work Items: #173

--- LoadedEmptyTotal.cs
+++ LoadedEmptyTotal.cs
@@ -1,4 +1,5 @@
 ﻿using CReiss.Core.Locations;
+using System.Collections.Generic;
 
 namespace CReiss.Core
 {
@@ -16,6 +17,23 @@ namespace CReiss.Core
         public bool IsLoaded { get; set; } = false;
         public double AvgTimeLoadToEmpty { get; set; } = 0;
         public double AvgTransitTimeFromDock { get; set; } = 0;
+        public double AvgDaysAtLocation
+        {
+            get
+            {
+                return this.UnitDwellTotals.Sum(x => x.TotalUnits) is int w && w != 0 ? this.UnitDwellTotals.Sum(x => x.TotalUnits * x.DaysAtLocation) / w : 0;
+            }
+        }
+        //
+        public List<UnitDwellTotal> UnitDwellTotals { get; private set; } = new List<UnitDwellTotal>();
+    }
+
+    public class UnitDwellTotal
+    {
+        public int LocationId { get; set; }
+        public int TotalUnits { get; set; }
+        public double DaysAtLocation { get; set; }
+        public bool IsLoaded { get; set; }
     }
 
 }
